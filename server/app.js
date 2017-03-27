00b617e6e2b4f0b2338a9acf2de88f3d58b51e81
Scott Bromander <scott@primeacademy.io>
2017-03-27 12:00:13 -0500

    adjustments to start script and name

--- server/app.js
+++ server/app.js
@@ -15,8 +15,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/public', '/views', 'index.html'));
 });
 
-app.listen(3000, function (req, res) {
-  console.log('Now listening on port 3000.');
-  console.log('Go to localhost:3000 to see site.');
+app.listen(5000, function (req, res) {
+  console.log('Now listening on port 5000.');
+  console.log('Go to localhost:5000 to see site.');
   console.log('Ctrl+C shuts down server.');
 });
